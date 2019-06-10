a9f99e2a97184e863232b7a10981618702e49ab0
Alisher Uluknazarov <alisheruluknazarov@gmail.com>
2019-06-10 23:41:15 +0500

    1 3 HW0 optional2

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -19,6 +19,5 @@ public class UserServlet extends HttpServlet {
 
 //        request.getRequestDispatcher("/users.jsp").forward(request, response);
         response.sendRedirect("users.jsp");
-        //test
     }
 }
